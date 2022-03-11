94c5413189644090047a18aa502b417c60c7acc5
Bruno Mendes <bruno.bmendes@mercadolivre.com>
2022-03-11 16:15:30 -0300

    resolvido desafio 5

--- pratica-integradora-1/src/com/company/Pessoa.java
+++ pratica-integradora-1/src/com/company/Pessoa.java
@@ -63,4 +63,27 @@ public class Pessoa {
     public void setAltura(double altura) {
         this.altura = altura;
     }
+
+    public int calcularIMC() {
+        double imc = this.peso/Math.pow(this.altura, 2);
+        if ( imc < 20) {
+            return -1;
+        } else if (imc < 25) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public boolean ehMaiorIdade() {
+        if ( this.idade >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return (this.nome + " tem " + this.idade + " anos. Pesa " + this.peso + "Kg e mede" + this.altura + "m.");
+    }
 }
