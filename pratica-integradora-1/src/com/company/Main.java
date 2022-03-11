45045813bfb45ea2c4687212da2843933405666d
Bruno Mendes <bruno.bmendes@mercadolivre.com>
2022-03-11 16:18:32 -0300

    resolvio deafio 6

--- pratica-integradora-1/src/com/company/Main.java
+++ pratica-integradora-1/src/com/company/Main.java
@@ -6,5 +6,12 @@ public class Main {
         Pessoa pessoa0 = new Pessoa();
         Pessoa pessoa1 = new Pessoa("Luiza", 23, "LULIS01");
 	    Pessoa pessoa2 = new Pessoa("Bruno", 27, "BRU01", 68, 1.78);
+
+        System.out.println(pessoa1.toString());
+	    System.out.println(pessoa1.ehMaiorIdade());
+
+        System.out.println(pessoa2.toString());
+        System.out.println(pessoa2.ehMaiorIdade());
+        System.out.println(pessoa2.calcularIMC());
     }
 }
