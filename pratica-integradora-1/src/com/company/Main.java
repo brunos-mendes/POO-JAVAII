4e16fde83b99db6a20b60ff2d728a7c87dee524b
Bruno Mendes <bruno.bmendes@mercadolivre.com>
2022-03-11 16:05:41 -0300

    resolvido desafios3 e 4

--- pratica-integradora-1/src/com/company/Main.java
+++ pratica-integradora-1/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Pessoa pessoa0 = new Pessoa();
+        Pessoa pessoa1 = new Pessoa("Luiza", 23, "LULIS01");
+	    Pessoa pessoa2 = new Pessoa("Bruno", 27, "BRU01", 68, 1.78);
     }
 }
